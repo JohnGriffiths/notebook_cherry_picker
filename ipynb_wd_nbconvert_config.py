73a5b06a99d2d2ea740b067a51e0fd6093e0e1f6
John Griffiths <j.davidgriffiths@gmail.com>
2014-07-23 06:40:49 -0400

    removed file; added new file with correct name

--- ipynb_wd_nbconvert_config.py
+++
@@ -1,5 +0,0 @@
-c = get_config()
-
-#Export all the notebooks in the current directory to the sphinx_howto format.
-c.Exporter.preprocessors = ['ipynb_wd_cherry_picking_preprocessor.CherryPickingPreprocessor']
-c.NbConvertApp.writer_class = 'notebook_copy_writer.NotebookCopyWriter'
